aa55f30298756296a00ba30b4b174dde721281e2
chaewon0824 <68092600+chaewon0824@users.noreply.github.com>
2020-10-06 01:08:32 +0900

    Update __init__.py

--- flask/__init__.py
+++ flask/__init__.py
@@ -1,5 +1,13 @@
-from flask import Flask
+from flask import Flask,g
 
 app = Flask(__name__)
+app.debug = True
 
-@app.route("/")
+@app.before_request
+def before_request():
+  g.str="한글" 
+  
+@app.route("/") #어디로 갈지 지정해주는 부분
+
+
+
