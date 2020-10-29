a204725450a01aa0134c2bfe0796222e72199336
chaewon0824 <68092600+chaewon0824@users.noreply.github.com>
2020-10-29 22:43:33 +0900

    Update __init__.py

--- flask/__init__.py
+++ flask/__init__.py
@@ -3,11 +3,13 @@ from flask import Flask,g
 app = Flask(__name__)
 app.debug = True
 
-@app.before_request
-def before_request():
-  g.str="한글" 
+
   
 @app.route("/") #어디로 갈지 지정해주는 부분
+def example():
+  return render_template("example.html")
 
 
+if __name__ == '__main__':
+  aap.run()
 
