75523153b4f7365cfc7ae4ae68ac263ed068651d
chaewon0824 <chae1_k@naver.com>
2020-11-17 16:05:09 +0900

    capston 1

--- __init__.py
+++ __init__.py
@@ -3,18 +3,22 @@ from flask_restful import Resource,Api
 
 app = Flask(__name__)
 app.debug = True
-#api = Api(app)
+api = Api(app)
 
 
 @app.route("/chatbot",methods=['POST'])
-def chatbot():
+def req1():
+  if request.method == 'POST':
+      textData = request.form['sentence']	
+  return "POST delivery {}".format(textData)
+
+@app.route("/")
+def example():
  # client = app.test_client()
  # response = client.get('/')
  # assert respnse.status_code == 200
  # text = ""
  # return render_template("example.html",textData=text)
-  request.endpoint
-  request.forms.
   return render_template("example.html")
 
 if __name__ == '__main__':
