7012e18c5cc620cb0b4de37e00aed1a96cbed402
chaewon0824 <68092600+chaewon0824@users.noreply.github.com>
2020-11-26 22:51:06 +0900

    Add files via upload

--- startflask.py
+++ startflask.py
@@ -1,9 +1,20 @@
 from flask import Flask,render_template,request,Response
-#from flask_restful import Resource,Api
+#from parlai.agents.local_human.local_human import *
+#from parlai.scripts.interactive import *
+#from flask_socketio import SocketIO,send,emit
+import socket
+
+
+HOST = '127.0.0.1'
+PORT =  5000
+BUF_SIZE = 1024
 
 app = Flask(__name__)
 app.debug = True
-#api = Api(app)
+#app.config['SECRET_KEY'] = 'secret'
+#socketio = SocketIO(app)
+
+
 
 @app.route("/test",methods=['POST'])
 def test():
@@ -21,14 +32,38 @@ def test():
   return jsonify(record)
 
 
+"""
+@socketio.on('my event')
+def handle_my_custom_event_recv(json,methods=['POST']):
+  print('received my event: '+str(json))
+
+def handle_my_custom_event_send(json,methods=['POST']):
+  socketio.emit('my reponse',json,callback=messageRecevied)
+"""
+
 
 @app.route("/chatbot",methods=['POST'])
 def req1():
   data = request.json
   sentence = data['sentence']
   print sentence
-  return "Success" + sentence
+  return sentence
 
+def answer_model(sentence):
+  s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+  s.bind((HOST,PORT))
+  s.listen()
+  c, addr = s.accept()
+  while True:
+        if(sentence):
+                c.sendall(sentence) #mirror_answer_to_model
+		text = c.recv(BUF_SIZE) #answer_from_model
+     		msg = text.decode()
+                if(sentence=='EXIT'):
+                        c.close()
+			s.close()
+                        break;
+  return msg
 
 
 @app.route("/example")
@@ -41,4 +76,5 @@ def example():
   return render_template("example.html")
 
 if __name__ == '__main__':
+ # socketio.run(app,host='0.0.0.0')
   app.run(host='0.0.0.0')
